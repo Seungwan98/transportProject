068f1812ae916c304f1b60067f625cb03ab18f7d
seungwan98 <sinna3210@gmail.com>
2024-05-31 14:13:04 +0900

    Initial Commit

---
+++ runningProject/runningProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  runningProjectApp.swift
+//  runningProject
+//
+//  Created by 양승완 on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct runningProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
